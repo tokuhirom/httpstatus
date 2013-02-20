16691ddd057895d4e313f2ea443c446ebdcc2932
tokuhirom <tokuhirom@gmail.com>
2013-02-21 05:30:28 +0900

    Checking in changes prior to tagging of version 0.0.1.
    Changelog diff is:

--- lib/App/httpstatus.pm
+++ lib/App/httpstatus.pm
@@ -4,3 +4,15 @@ use warnings;
 use 5.008005;
 our $VERSION = '0.0.1';
 1;
+__END__
+
+=head1 NAME
+
+App::httpstatus - httpstatus
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
